b5921845c46e63ae1e9695fa7f46c9e0ebc16eae
FurkanOzkaya <48178781+FurkanOzkaya@users.noreply.github.com>
2020-08-05 22:35:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'easy_differ',
   packages = ['easy_differ'],
-  version = '0.0.1',
+  version = '0.1',
   license='MIT',
   description = "Compare Text line by line. When your texts has different place in file you can't use beyond compare totaly so you may need to compare line by line.",   # Give a short description about your library
   author = 'Furkan Ozkaya',
   author_email = 'furkanozkaya45@gmail.com',
   url = 'https://github.com/FurkanOzkaya/easy_differ',
-  download_url = 'https://github.com/FurkanOzkaya/easy_differ/archive/0.0.1.tar.gz',
+  download_url = 'https://github.com/FurkanOzkaya/easy_differ/archive/0.1.tar.gz',
   keywords = ['DIFFER', 'COMPARE', 'TEXT COMPARE', "ARRAY COMPARE"],
   long_description=long_description,
   long_description_content_type='text/markdown',
